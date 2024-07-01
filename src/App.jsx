2bea4b9c3b42ba70a744b35b11bd0f3f8c8946fa
theboss17121999 <46495612+theboss17121999@users.noreply.github.com>
2024-07-01 22:58:52 +0530

    test 5

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ class ErrorBoundary extends React.Component {
 
 function App() {
   return (
-    <BrowserRouter basename="/SuperTicTacToe">
+    <BrowserRouter basename="/SuperTicTacToe/">
       <RecoilRoot>
         <ErrorBoundary>
           <Routes>
